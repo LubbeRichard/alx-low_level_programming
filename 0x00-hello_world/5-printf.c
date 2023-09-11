3f8f635ea15af6aa518f006a1a3dac404f96c9c8
LubbeRichard <lubberich5@gmail.com>
2023-09-11 00:16:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * function that prints text using printf
+ * Return: - Return 0 is a success
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
