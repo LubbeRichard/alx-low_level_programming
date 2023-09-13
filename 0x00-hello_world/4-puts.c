e68cf5e7d885d4bfe587a6ed9cf4acb19be6b452
LubbeRichard <lubberich5@gmail.com>
2023-09-13 06:41:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This function prints text using puts
+ * Return: Return 0 is success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
